9fcdb488fef39b6c008072ec736cd4e5660ae2e2
Noah Glusenkamp <noah.glusenkamp@gmail.com>
2017-09-25 15:45:45 -0700

    add getShoreline() cleanup ShorelineService

--- src/app/shoreline.service.ts
+++ src/app/shoreline.service.ts
@@ -1,26 +1,32 @@
 import { Injectable } from '@angular/core';
 
+//Geojson file exported from QGIS source shapefile via email from SDOT
 import shorelinesData from '../data/ShorelineStreetEndsLayer.json';
 
 @Injectable()
 export class ShorelineService {
-  shorelines;
-  shorelinesGeojson;
+
+  idFieldname = 'NO_';
+
+  public getShorelinesGeojson() {
+      return shorelinesData;
+  }
 
   public getShorelines() {
-      // return an array of shoreline locations not geojson object
-      this.shorelines = shorelinesData['features'];
-      return this.shorelines;
-  }
-
-    public getShorelinesGeojson() {
-      this.shorelinesGeojson = shorelinesData;
-      return this.shorelinesGeojson;
-  }
+    // return an array of shoreline locations not geojson object
+      return shorelinesData['features'];
+}
 
   public getShoreline(id) {
-    console.log(this.shorelines);
-    return id;
+      return this.getSingleShorelineFromGeojson(id, this.idFieldname);
+  }
+
+  private getSingleShorelineFromGeojson(id, idFieldname) {
+    // pluck individual shoreline location from geojson object
+    for(var i = 0; i < shorelinesData.features.length; i++) { 
+      if(shorelinesData.features[i].properties[idFieldname] == id)
+      return shorelinesData.features[i].properties
+    }
   }
 
   constructor() { }
